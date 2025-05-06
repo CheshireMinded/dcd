b6906e1a57980cf5578a733e65f10118b9736d62
Lauren Hall <105026157+CheshireMinded@users.noreply.github.com>
2025-05-06 12:34:39 -0700

    Add files via upload

---
+++ setup_venv.sh
@@ -0,0 +1,8 @@
+# setup_venv.sh
+#!/bin/bash
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install faker
+pip install fastapi uvicorn python-dotenv
+uvicorn dynamic_response:app --host 0.0.0.0 --port 8080
